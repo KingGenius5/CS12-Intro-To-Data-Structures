1fe20a6e306a3525a07db212bc352a125699e114
Muhammad Tifak <moht20009@gmail.com>
2020-01-24 18:49:06 -0800

    Added python_quote module

---
+++ quote_user.py
@@ -0,0 +1,5 @@
+import python_quote as pquote
+
+
+if __name__ == "__main__":
+    print(pquote.random_python_quote()) 
---
+++ rearrange.py
@@ -0,0 +1,28 @@
+import sys
+import random as r
+
+
+def shuffle_list(words_list):
+    """
+    Shuffles order of words in a given list
+    """
+    for i, word in enumerate(words_list):
+        random_index = r.randint(0, len(words_list) - 1)
+        words_list[i] = words_list[random_index]
+        words_list[random_index] = word
+
+    return words_list
+
+
+if __name__ == "__main__":
+    words_list = []
+
+    #Add args to list
+    for arg in range(1, len(sys.argv)):
+        words_list.append(sys.argv[arg])
+    
+    print(words_list)#adds the first few letters
+
+    words_list = shuffle_list(words_list)
+
+    print(words_list)#adds the new rearranged list 
