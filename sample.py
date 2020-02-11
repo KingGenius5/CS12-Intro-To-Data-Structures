08c1e2bd27859620a9fcb8717fc92bff759f0cf1
Muhammad Tifak <moht20009@gmail.com>
2020-02-10 19:14:28 -0800

    Added starter code for stochastic sampling

---
+++ sample.py
@@ -0,0 +1,11 @@
+text = 'one fish two fish red fish blue fish'
+word_counts = {'one': 1, 'fish': 4, 'two': 1,
+               'red': 1, 'blue': 1}
+               
+               
+def sample_by_frequency(histogram):
+  
+    # TODO: select a word based on frequency
+    #how can we sample words using observed frequencies?
+    
+    return word
