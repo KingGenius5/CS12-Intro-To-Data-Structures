93bee3be2c9d6cd7cf0f45d7ffb140e9a231d55c
Muhammad Tifak <moht20009@gmail.com>
2020-02-10 21:48:28 -0800

    Created Flask route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from sample import *
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def main_tweet_page():
+    
+    words = read_file('fish.txt')
+    hist = histogram_dictonary(words)
+    token_count = len(words)
+    sent = sentence_gen(8, token_count, hist)
+
+    return sent
