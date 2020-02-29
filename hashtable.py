bb495d6ddc612837e385f86a2ab19f9ba6a28bc3
Muhammad Tifak <moht20009@gmail.com>
2020-02-29 15:12:13 -0800

    Implemented keys & length method

--- hashtable.py
+++ hashtable.py
@@ -39,6 +39,12 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Collect all values in each bucket
+        all_values = []
+        for bucket in self.buckets:
+            for key, value in bucket.items():
+                all_values.append(value)
+
+        return all_values
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
@@ -54,6 +60,12 @@ class HashTable(object):
         TODO: Running time: O(???) Why and under what conditions?"""
         # TODO: Loop through all buckets
         # TODO: Count number of key-value entries in each bucket
+        items_count = 0
+        for bucket in self.buckets:
+            for item in bucket.items():
+                items_count +=1
+
+        return items_count
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
