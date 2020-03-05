ed7adf90c1e54031e889a69cc19719e09a1d82e8
Muhammad Tifak <moht20009@gmail.com>
2020-03-04 21:57:41 -0800

    Updated flask route for second order markov chain

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from sample import *
+from markov import *
+import random
+from dictogram import Dictogram
+from histogram import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def main_tweet_page():
     
-    words = read_file('fish.txt')
-    hist = histogram_dictonary(words)
-    token_count = len(words)
-    sent = sentence_gen(8, token_count, hist)
+    words = read_file('manifesto.txt')
+    sent = higher_order_walk(words, 40)
 
     return sent
